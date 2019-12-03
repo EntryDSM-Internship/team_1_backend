e0131e230bc7666bf2b24aad2c54d2c48fe64d42
yangseungbin <tanja1206@naver.com>
2019-12-03 22:24:25 +0900

    post db

---
+++ src/models/post.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Post = new Schema({
+    nick: String,
+    content: String,
+    good: Number,
+});
+
+Post.statics.create = function (nick, content) {
+    const post = new this({
+        nick,
+        content,
+    });
+
+    return post.save();
+}
+
+Post.statics.findOneById = function(_id) {
+    return this.findOne({
+        _id
+    }).exec();
+}
+
+module.exports = mongoose.model('Post', Post);
