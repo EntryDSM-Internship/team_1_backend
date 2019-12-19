dfa90a11974f228451abd48cc15e45684ebc4614
yangseungbin <tanja1206@naver.com>
2019-12-19 20:18:39 +0900

    index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const user = require('./user');
 const post = require('./post');
+const follow = require('./follow');
 
 router.use('/user', user);
 router.use('/post', post);
+router.use('/follow', follow);
 
 module.exports = router;
