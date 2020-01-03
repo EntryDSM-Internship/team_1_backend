2de4cd5c4c739cd2983b4ee265d8242415c30aff
yangseungbin <tanja1206@naver.com>
2020-01-03 22:48:51 +0900

    main

--- src/app.js
+++ src/app.js
@@ -4,18 +4,19 @@ const logger = require('morgan');
 const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
+const path = require('path');
 
 const app = express();
-
 const config = require('./config');
-
 const swaggerDoc = YAML.load('./apidocs/swagger.yaml');
-
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(logger('dev'));
 app.use(cors());
+
+app.use('/profile', express.static(path.join(__dirname, 'uploads/profile')));
+app.use('/post', express.static(path.join(__dirname, 'uploads/post')));
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 app.use('/api', require('./routes'));
@@ -24,7 +25,7 @@ app.set('jwt-secret', config.secretKey);
 app.set('refresh-secret', config.refreshKey);
 
 app.use((err, req, res, next) => {
-    res.status(404).json(err);
+    res.status(404).json(err.message);
 });
 
 app.listen(3000, () => {
