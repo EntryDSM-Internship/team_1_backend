8c508e589ed550486edd63f05c68327394c5abe6
yangseungbin <tanja1206@naver.com>
2020-01-03 22:48:43 +0900

    index

--- src/routes/follow/index.js
+++ src/routes/follow/index.js
@@ -3,9 +3,10 @@ const controller = require('./follow.controller');
 const authMiddleware = require('../../middlewares/auth');
 
 router.get('/following/:nick', authMiddleware, controller.addFollowing);
-router.get('/list', authMiddleware, controller.allowList);
+router.get('/allowlist', authMiddleware, controller.allowList);
 router.get('/allow/:nick', authMiddleware, controller.allowFollowing, controller.addFollower);
 router.get('/reject/:nick', authMiddleware, controller.rejectFollowing);
 router.delete('/unfollow/:nick', authMiddleware, controller.unFollow, controller.unFollower);
+router.get('/followlist', authMiddleware, controller.followList);
 
 module.exports = router;
--- src/routes/main/index.js
+++ src/routes/main/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const controller = require('./main.controller');
 const authMiddleware = require('../../middlewares/auth');
 
-router.get('/', authMiddleware, controller.showPost);
+router.get('/', authMiddleware, controller.showMe);
+router.get('/post', authMiddleware, controller.showPost);
 
 module.exports = router;
